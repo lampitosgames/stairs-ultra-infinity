29f35270b7408e7e410bb9a0d112f633223b60ad
Jake <jxt8029@rit.edu>
2017-11-16 01:00:12 -0500

    Added death barrier with position reset. Added primative components (without scripts) to scene. Made prefabs folder and added two primatives for platforms and stairs.

---
+++ Assets/Scripts/DeathBarrier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DeathBarrier : MonoBehaviour {
+
+    GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.transform.position.y < 25)
+        {
+            player.transform.position = new Vector3(62, 39, -9);
+        }
+	}
+    
+}
