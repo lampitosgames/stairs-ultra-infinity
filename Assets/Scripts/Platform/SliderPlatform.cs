612a93f88efa3161635611ccdebcadf23cdbfe55
MAIN\mvr9788 <mike4ruls@gmail.com>
2017-12-05 18:55:40 -0500

    Upgrades sliding platforms, Can now add as many points for the platform to traverse to, Can also set if you want it to loop around to the beginning point ot not, Can also set wether you want the platform to continously move without the player on it or not

--- Assets/Scripts/Platform/SliderPlatform.cs
+++ Assets/Scripts/Platform/SliderPlatform.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SliderPlatform : MonoBehaviour {
-    public GameObject startPoint;
-    public GameObject endPoint;
+    public GameObject movePoints;
 
-    List<GameObject> points;
+    public List<GameObject> points;
 
     Platform myPlatform;
 
@@ -14,10 +13,14 @@ public class SliderPlatform : MonoBehaviour {
     public float platformRadiusDetection;
     public float pointRadiusDetection;
 
-    int currentPoint;
+    public bool loop;
+    public bool continous;
 
-    bool foward;
+
+    int currentPoint;
+    int nextPoint;
     bool currentlyMoving;
+    bool foward;
 
     // Use this for initialization
     void Start () {
@@ -25,50 +28,88 @@ public class SliderPlatform : MonoBehaviour {
         currentlyMoving = false;
         myPlatform = this.GetComponent<Platform>();
 
-        for (int i = 1; i < this.transform.childCount; i++)
+        currentPoint = 0;
+        nextPoint = 1;
+
+        for (int i = 0; i < movePoints.transform.childCount; i++)
         {
-            points.Add(this.transform.GetChild(i).gameObject);
+            points.Add(movePoints.transform.GetChild(i).gameObject);
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (myPlatform.active || currentlyMoving)
+        if ((myPlatform.active || currentlyMoving) || continous)
         {
             Vector3 dir = Vector3.zero;
             Vector3 distAway = Vector3.zero;
-            if (foward)
-            {
-                dir = endPoint.transform.position - startPoint.transform.position;
-                distAway = endPoint.transform.position - transform.position;
-            }
-            else
-            {
-                dir = startPoint.transform.position - endPoint.transform.position;
-                distAway = startPoint.transform.position - transform.position;
-            }
 
-            //if (foward)
-            //{
-            //    dir = points[currentPoint + 1].transform.position - points[currentPoint].transform.position;
-            //    distAway = points[currentPoint + 1].transform.position - this.transform.position;
-            //}
-            //else
-            //{
-            //    dir = points[currentPoint].transform.position - points[currentPoint + 1].transform.position;
-            //    distAway = points[currentPoint].transform.position - transform.position;
-            //}
+            dir = points[nextPoint].transform.position - points[currentPoint].transform.position;
+            distAway = points[nextPoint].transform.position - this.transform.position;
 
             dir.Normalize();
             transform.position += (dir * travelSpeed) * Time.deltaTime;
 
             if (distAway.sqrMagnitude <= (Mathf.Pow(platformRadiusDetection, 2.0f) + Mathf.Pow(pointRadiusDetection, 2.0f)))
             {
-                foward = foward ? false : true;
-                if (!myPlatform.active)
+                if (foward)
                 {
-                    currentlyMoving = false;
+                    currentPoint++;
+                    nextPoint++;
+
+                    if (currentPoint >= movePoints.transform.childCount - 1)
+                    {
+                        if (loop)
+                        {
+                            if (currentPoint == movePoints.transform.childCount)
+                            {
+                                currentPoint = 0;
+                            }
+                            else
+                            {
+                                nextPoint = 0;
+                            }
+                        }
+                        else
+                        {
+                            foward = foward ? false : true;
+                            nextPoint = currentPoint - 1;
+                        }
+                    }
+                    if (!myPlatform.active)
+                    {
+                        currentlyMoving = false;
+                    }
                 }
+                else
+                {
+                    currentPoint--;
+                    nextPoint--;
+
+                    if (currentPoint <= 0)
+                    {
+                        if (loop)
+                        {   
+                            if(currentPoint == -1)
+                            {
+                                currentPoint = movePoints.transform.childCount - 1;
+                            }
+                            else
+                            {
+                                nextPoint = movePoints.transform.childCount - 1;
+                            }
+                        }
+                        else
+                        {
+                            foward = foward ? false : true;
+                            nextPoint = currentPoint + 1;
+                        }
+                    }
+                    if (!myPlatform.active)
+                    {
+                        currentlyMoving = false;
+                    }
+                }  
             }
             else
             {
